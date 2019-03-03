e01405e1e26ec934e0b9eb7401a3b711eee258c8
kobutya7 <makoto.saeki.0627@gmail.com>
2019-03-03 16:11:43 +0900

    add test db & react

--- containers/postgresql/init.sh
+++ containers/postgresql/init.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e
 
-# initdbでdockerというユーザとデータベースを作成する
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d "dev" <<-EOSQL
     GRANT ALL PRIVILEGES ON DATABASE dev TO devuser;
     CREATE DATABASE test;
